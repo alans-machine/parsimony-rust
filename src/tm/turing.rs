9bc1df5c5d6a1f636bc913ebac3c3b7605230a28
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-04-15 17:02:43 +0200

    Document Turing Machine

--- src/tm/turing.rs
+++ src/tm/turing.rs
@@ -4,6 +4,7 @@
 use super::transition::Transitions;
 use super::tape::ConcreteTape;
 
+/// The actual Turing machine
 pub struct Machine<S, Q> where S: Clone {
     state: Q,
     tape: ConcreteTape<S>,
