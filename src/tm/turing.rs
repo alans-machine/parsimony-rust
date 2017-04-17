81a984d94be8544b5c3d55bbbdd5b84e68b80a73
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-04-17 08:50:33 +0200

    Test step function

--- src/tm/turing.rs
+++ src/tm/turing.rs
@@ -14,6 +14,15 @@ pub struct Machine<Q, S> where Q: Clone + Eq + Hash, S: Clone + Eq + Hash  {
 }
 
 impl <Q, S> Machine<Q, S> where S: Clone + Eq + Hash, Q: Clone + Eq + Hash {
+    /// Create a Turing machine from a initial state, a tape and transistions
+    pub fn new(q: Q, tape: ConcreteTape<S>, transitions: Transitions<Q, S>) -> Machine<Q, S> {
+        Machine {
+            state: q,
+            tape: tape,
+            transitions: transitions,
+        }
+    }
+
     /// Step the machine through one transition.
     pub fn step(&self) -> Option<Machine<Q, S>> {
         let symbol = self.tape.read();
@@ -31,6 +40,43 @@ impl <Q, S> Machine<Q, S> where S: Clone + Eq + Hash, Q: Clone + Eq + Hash {
                 })
             }
             None => None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::movement::Movement;
+    use super::super::tape::TapeBuilder;
+    use super::super::transition::{Transitions, TransitionKey, TransitionValue};
+
+    #[test]
+    fn should_step_through_a_program() {
+        let m = Machine::new(
+            0,
+            TapeBuilder::with_blank("_")
+                .with_current("I")
+                .with_right_tape(vec!["I", "I"])
+                .build(),
+            Transitions::new()
+                .insert(
+                    TransitionKey::new(0, "I"),
+                    TransitionValue::new(0, "I", Movement::Right))
+                .insert(
+                    TransitionKey::new(0, "_"),
+                    TransitionValue::new(1, "I", Movement::Left))
+                .insert(
+                    TransitionKey::new(1, "I"),
+                    TransitionValue::new(1, "I", Movement::Left))
+                .insert(
+                    TransitionKey::new(1, "_"),
+                    TransitionValue::new(2, "_", Movement::Right))
+        );
+
+        match m.step() {
+            Some(_) => assert!(true),
+            None => assert!(false),
         }
     }
 }
